79e30fbb264a96e6b66a6530a5cc57bd8e6e73b7
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-07-11 21:28:14 +0200

    fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,26 @@
 # Fish shell
 
+egrep "^export " ~/.profile | while read e
+	set var (echo $e | sed -E "s/^export ([A-Z_]+)=(.*)\$/\1/")
+	set value (echo $e | sed -E "s/^export ([A-Z_]+)=(.*)\$/\2/")
+	
+	# remove surrounding quotes if existing
+	set value (echo $value | sed -E "s/^\"(.*)\"\$/\1/")
 
+	if test $var = "PATH"
+		# replace ":" by spaces. this is how PATH looks for Fish
+		set value (echo $value | sed -E "s/:/ /g")
+	
+		# use eval because we need to expand the value
+		eval set -xg $var $value
+
+		continue
+	end
+
+	# evaluate variables. we can use eval because we most likely just used "$var"
+	set value (eval echo $value)
+
+	#echo "set -xg '$var' '$value' (via '$e')"
+	set -xg $var $value
+end
 
